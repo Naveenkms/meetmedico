9c4c29a83cf149b4b098d03358c86237690cd2a4
Naveenkms <nvnkms123@gmail.com>
2026-07-07 15:07:36 +0530

    feat: add animation for header menu

--- app/(landing-page)/_components/header.tsx
+++ app/(landing-page)/_components/header.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 import NavContainer from "./nav-container"
 import Profile from "./profile"
@@ -14,10 +16,37 @@ import {
 } from "@/components/ui/dropdown-menu"
 import NavLinksDropdownTrigger from "./nav-links-dropdown-trigger"
 import NavLogo from "./nav-logo"
+import { cn } from "@/lib/utils"
 
 export default function Header() {
+  const [isVisible, setIsVisible] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
+  console.log("ss", isVisible)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        setIsVisible(false)
+      } else {
+        setIsVisible(true)
+      }
+
+      setLastScrollY(currentScrollY)
+    }
+
+    window.addEventListener("scroll", handleScroll, { passive: true })
+
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [lastScrollY])
   return (
-    <header className="sticky top-0 z-50 flex h-15 items-center border-b border-neutral-50 bg-primary/5 px-4 py-2 backdrop-blur-[44px] md:bg-white-900/80 md:py-3">
+    <header
+      className={cn(
+        "fixed top-0 left-0 z-50 flex h-15 w-full items-center border-b border-neutral-50 bg-primary/5 px-4 py-2 backdrop-blur-[44px] transition-transform duration-300 ease-in-out md:bg-white-900/80 md:py-3",
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      )}
+    >
       <NavContainer>
         <div className="hidden items-center md:flex md:basis-[66.66%] md:*:basis-1/2">
           <nav className="hidden md:flex">
