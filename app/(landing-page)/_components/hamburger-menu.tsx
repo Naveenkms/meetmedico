67f2f14081df268fe759c092bcf2bd13f6636add
Naveenkms <nvnkms123@gmail.com>
2026-07-18 16:49:30 +0530

    chore: add sheet component

--- app/(landing-page)/_components/hamburger-menu.tsx
+++ app/(landing-page)/_components/hamburger-menu.tsx
@@ -1,15 +1,35 @@
 import { cn } from "@/lib/utils"
 import Hamburger from "@/components/icons/hamburger"
 import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 export default function HamburgerMenu({ className }: { className?: string }) {
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      className={cn("rounded-lg bg-inherit p-2.75 lg:hidden", className)}
-    >
-      <Hamburger />
-    </Button>
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className={cn("rounded-lg bg-inherit p-2.75 lg:hidden", className)}
+        >
+          <Hamburger />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="data-[side=left]:w-full">
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>This action cannot be undone.</SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
   )
 }
