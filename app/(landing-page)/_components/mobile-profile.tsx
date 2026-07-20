9cc45831e7486776835369afb6a65dffd2945f62
Naveenkms <nvnkms123@gmail.com>
2026-07-20 12:34:35 +0530

    feat: add drawer component for profile component

--- app/(landing-page)/_components/mobile-profile.tsx
+++ app/(landing-page)/_components/mobile-profile.tsx
@@ -1,10 +1,35 @@
 import User from "@/components/icons/user"
 import { Button } from "@/components/ui/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 
 export default function MobileProfile() {
   return (
-    <Button className="size-10 p-2.5 md:hidden">
-      <User />
-    </Button>
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button className="size-10 p-2.5 md:hidden">
+          <User />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Register or Login</DrawerTitle>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button variant="outline">Register</Button>
+          <DrawerClose asChild>
+            <Button>Login</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   )
 }
